5fe3c54da2de29980d16b131498155a5a95f2d20
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-08-30 12:36:05 +0200

    Update head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,19 +2,19 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="I'm Ndungutse Charles, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
+      <meta name="description" content="I'm Ndungutse Charles, full-stack developer based in Kigali, Rwanda. My journey in web development began during my first year of upper-high school." />
       <meta name="keywords" content="Ndungutse Charles, portfolio, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
       <meta property="og:site_name" content="Ndungutse Charles's Portfolio" />
       <meta property="og:title" content="Portfolio | Ndungutse Charles - Full Stack Developer" />
       <meta property="og:description"
-        content="I'm Ndungutse Charles, full-stack developer based in Pune, India. My journey in web development began during my first year of college." />
-      <meta property="og:url" content="https://ndungutsecharles.vercel.app" />
+        content="I'm Ndungutse Charles, full-stack developer based in Kigali, Rwanda. My journey in web development began during my first year of upper-high school." />
+      <meta property="og:url" content="https://ndungutsecharles.vercel.app/avatar.png" />
       <meta property="og:image" content="https://ndungutsecharles.vercel.app/avatar.png" />
       <link rel="canonical" href="https://ndungutsecharles.vercel.app" />
       <meta name="author" content="Ndungutse Charles" />
       <meta name="language" content="English" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="https://ndungutsecharles.vercel.app" />
+      <meta name="twitter:site" content="@ndungutsecharl1" />
       <meta name="application-name" content="Portfolio | Ndungutse Charles - Full Stack Developer" />
       <meta name="apple-mobile-web-app-title" content="Portfolio | Ndungutse Charles - Full Stack Developer" />
       <link rel="icon" href="/favicon.ico" />
