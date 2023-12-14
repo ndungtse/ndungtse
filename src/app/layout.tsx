65ec74553b52e5e2c698dbdee3973aa8fdbe86ba
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-12-14 17:37:36 +0200

    Add speed insight for speed monitoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-'use client';
-import './globals.css'
-import { Poppins } from '@next/font/google'
-import { ThemeProvider } from 'next-themes'
-import { Analytics } from '@vercel/analytics/react';
+"use client";
+import "./globals.css";
+import { Poppins } from "@next/font/google";
+import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700', '800'],
-  variable: '--font-poppins'
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head />
-      <ThemeProvider attribute='class' defaultTheme='light'>
-        <body className={`${poppins.className} font-poppins bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <body
+          className={`${poppins.className} font-poppins bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden`}
+        >
           {/* <body className='bg-gray-100/50 dark:bg-grey-900 text-black dark:text-white overflow-x-hidden'> */}
           {children}
           <Analytics />
+          <SpeedInsights />
         </body>
       </ThemeProvider>
     </html>
-  )
+  );
 }
